9a442f1dc933b2740933c443979cbaaeb5fe63db
ioanabedreag <ioanapdv@yahoo.com>
2023-02-05 21:22:22 +0200

    Removed the horizontal scroll view

--- MovieManagerApp/app/src/main/java/com/example/moviemanager/HomeFragment.java
+++ MovieManagerApp/app/src/main/java/com/example/moviemanager/HomeFragment.java
@@ -26,27 +26,10 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
 
-//        HorizontalScrollView horizontalScrollView = view.findViewById(R.id.horizontalScrollView);
-//        LinearLayout linearLayout = view.findViewById(R.id.linear_layout);
-//
-//        String[] imageUrls = {
-//                "https://images.unsplash.com/photo-1542614471-001ccf2b449c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-//                "https://images.unsplash.com/photo-1542614471-001ccf2b449c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-//                "https://images.unsplash.com/photo-1542614471-001ccf2b449c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-//                "https://images.unsplash.com/photo-1542614471-001ccf2b449c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-//                "https://images.unsplash.com/photo-1542614471-001ccf2b449c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-//                "https://images.unsplash.com/photo-1542614471-001ccf2b449c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-//        };
-//
-//        for (String imageUrl : imageUrls) {
-//            ImageView imageView = (ImageView) inflater.inflate(R.layout.image_view_item , linearLayout, false);
-//            Picasso.get().load(imageUrl).into(imageView);
-//            linearLayout.addView(imageView);
-//        }
+
         Button goToMovies = view.findViewById(R.id.goToMoviesBtn);
 
         goToMovies.setOnClickListener(new View.OnClickListener() {
